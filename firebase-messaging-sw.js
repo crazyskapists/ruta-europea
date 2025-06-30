ec65358d1009ee9efc440b4e33d2f00511c29922
crazyskapists <crazyskapists@gmail.com>
2025-06-30 22:32:03 +0200

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,12 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyBw1GKqP2m3QJxlEnolzrUjJ9w3d2ta9jA",
+  authDomain: "europita-465eb.firebaseapp.com",
+  projectId: "europita-465eb",
+  storageBucket: "europita-465eb.firebasestorage.app",
+  messagingSenderId: "458801176847",
+  appId: "1:458801176847:web:01280c38b419de5d3c5e7a",
+  measurementId: "G-WYHDXMGTR6"
 });
 
 const messaging = firebase.messaging();
